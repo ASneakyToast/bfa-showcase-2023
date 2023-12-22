b8b8ec84b380b59eccd5d0984cde2858ab1ec81b
asneakytoast <joelnotlithgow@gmail.com>
2023-12-21 16:14:08 -0800

    Github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://staging-cca-bfa-exhibition-2023.joellithgow.com',
   integrations: [svelte()]
 });
